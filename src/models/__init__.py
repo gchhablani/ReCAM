fd79ecb33090780a87e3cf145c84ccb0588be215
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-01-13 11:45:08 +0530

    Fix bugs

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -6,3 +6,4 @@ from src.models.roberta_cloze import *
 from src.models.albert_cloze import *
 from src.models.distilbert_cloze import *
 from src.models.bert_answer_attention import *
+from src.models.gsabert_answer_attention import *
