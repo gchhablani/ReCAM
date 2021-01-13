d10709cba6f927cb0f4e1f649ecbb7bc69f1e4f8
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-01-13 22:40:25 +0530

    Add ClozePreprocessor

--- src/modules/preprocessors.py
+++ src/modules/preprocessors.py
@@ -48,35 +48,35 @@ class GlovePreprocessor(Preprocessor):
         return model, train_dataset, val_dataset
 
 
-# @configmapper.map("preprocessors", "clozePreprocessor")
-# class ClozePreprocessor(Preprocessor):
-#     """GlovePreprocessor."""
+@configmapper.map("preprocessors", "clozePreprocessor")
+class ClozePreprocessor(Preprocessor):
+    """GlovePreprocessor."""
 
-#     def __init__(self, config):
-#         """
-#         Args:
-#             config (src.utils.module.Config): configuration for preprocessor
-#         """
-#         super(ClozePreprocessor, self).__init__()
-#         self.config = config
-#         self.tokenizer = configmapper.get_object(
-#             "tokenizers", self.config.main.preprocessor.tokenizer.name
-#         ).from_pretrained(
-#             **self.config.main.preprocessor.tokenizer.init_params.as_dict()
-#         )
+    def __init__(self, config):
+        """
+        Args:
+            config (src.utils.module.Config): configuration for preprocessor
+        """
+        super(ClozePreprocessor, self).__init__()
+        self.config = config
+        self.tokenizer = configmapper.get_object(
+            "tokenizers", self.config.main.preprocessor.tokenizer.name
+        ).from_pretrained(
+            **self.config.main.preprocessor.tokenizer.init_params.as_dict()
+        )
 
-#     def preprocess(self, model_config, data_config):
-#         train_dataset = configmapper.get_object("datasets", data_config.main.name)(
-#             data_config.train, self.tokenizer
-#         )
-#         val_dataset = configmapper.get_object("datasets", data_config.main.name)(
-#             data_config.val, self.tokenizer
-#         )
-#         model = configmapper.get_object("models", model_config.name).from_pretrained(
-#             **model_config.params.as_dict()
-#         )
+    def preprocess(self, model_config, data_config):
+        train_dataset = configmapper.get_object("datasets", data_config.main.name)(
+            data_config.train, self.tokenizer
+        )
+        val_dataset = configmapper.get_object("datasets", data_config.main.name)(
+            data_config.val, self.tokenizer
+        )
+        model = configmapper.get_object("models", model_config.name).from_pretrained(
+            **model_config.params.as_dict()
+        )
 
-#         return model, train_dataset, val_dataset
+        return model, train_dataset, val_dataset
 
 
 @configmapper.map("preprocessors", "transformersPreprocessor")
